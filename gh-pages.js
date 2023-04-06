1b4bf4cd2c4a6cef79662a9a555286bae2330c00
Daniel Bishop <daniel@engmalinguistics.com>
2023-04-06 14:37:18 -0500

    initial gh-pages commit

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/engma-linguistics/engma-linguistics.github.io.git', // Update to point to your repository  
+        user: {
+            name: 'Daniel Bishop', // update to use your name
+            email: 'daniel@engmalinguistics.com' // Update to use your email
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
